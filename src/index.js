91746cfbe00fbb1d8bd59889a627b889e55987aa
Mariana <marianamorais.dev@gmail.com>
2019-12-13 23:51:32 -0300

    :construction: Fix

--- src/index.js
+++ src/index.js
@@ -1,33 +1,11 @@
-import { config } from 'dotenv';
-import { Client, RichEmbed } from 'discord.js';
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token} = require('config.json');
 
-config();
-const { TOKEN_DISCORD } = process.env
-const client = new Client();
+const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
-    console.log(`Exit ${client.user.tag}: ctrl + c`);
-});
-
-client.on('message', ( msg ) => {
-    const { content } = msg
-    const commands = {
-    "!invite": 'https://discord.gg/nHf4AcU',
-    "!gitrep": 'https://github.com/marianamorais/CatHack',
-};
-    commands[content] && msg.reply(commands[content]);
-});
-
-client.on('message', (message) => {
-    if(message.content === 'embed') {
-    const embed = new RichEmbed()
-
-        .setTitle('A slick little embed')
-        .setColor(0xFF0000)
-        .setDescription('Hello, this is a slick embed!');
-      message.channel.send(embed);
-    }
+client.once('ready', () => {
+    console.log('Meow!');
 });
   
-client.login(TOKEN_DISCORD)
+client.login(token)
