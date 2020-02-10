c7924f2751ee0ca32319cdd4183980f9d04cbeac
Mariana <marianamorais.dev@gmail.com>
2020-02-09 23:35:27 -0300

    stuff

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -2,6 +2,9 @@ const { RichEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
 const { getMember, formatDate } = require("../../functions.js");
 
+const moment = require('moment')
+moment.locale('pt-br')
+
 module.exports = {
     name: "userinfo",
     description: "Retorna informações do usuário",
@@ -9,35 +12,30 @@ module.exports = {
 
     run: (client, message, args) => {
         const member = getMember(message, args.join(" "));
+
+        //Member variaveis
+        const membercreated = formatDate(member.user.createdAt);
+        const memberjoined = formatDate(member.joinedAt);
         const inline = true
+
         const status = {
-            online: "<Online",
+            online: "Online",
             idle: "Ausente",
             dnd: "Não pertubar",
             offline: "<Offline/Invisivel"
           }
-        const date = member.user.createdAt
-
+   
         // Member variables
-        const memberJoined = formatDate(member.joinedAt);
-        const roles = member.roles
-            .filter(r => r.id !== message.guild.id)
-            .map(r => r).join(", ") || 'none';
-
-
-            // User variables
 
         const embed = new RichEmbed()
             .setThumbnail(member.user.displayAvatarURL)
             .setColor(member.displayHexColor === '#000000' ? '#ffffff' : member.displayHexColor)
 	        .setTitle(`:cat: ${member.displayName}`)
-            // .setDescription('**Informações do usuário**')
             .addField("**Username**", `${member.user.tag}`, inline)
             .addField("**ID**", member.user.id, inline)
             
-            .addField("**Criou em**", member.user.createdAt)
-            .addField("Criou em", formatDate('DD/MM/YYYY, às HH:mm:ss', date))
-            .addField("**Entrou em:**", message.member.joinedAt)
+            .addField("**Criou em**", membercreated)
+            .addField("**Entrou em:**", memberjoined)
             .addField("**Cargos**", `${member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "Sem cargos"}`, true)
             .addField("Online a", moment().to(client.startTime, true))
 
