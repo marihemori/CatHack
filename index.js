89f3219e3533ba9bb93db5dea400e688642b6cda
Mariana <marianamorais.dev@gmail.com>
2020-02-11 21:10:48 -0300

    stuff

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const { config } = require("dotenv");
-const Discord = require('discord.js')
-const client = new Discord.Client()
-const Enmap = require('enmap')
+if (process.version.slice(1).split('.')[0] < 8) throw new Error('Node 8.0.0 or higher is required. Update Node on your system.')
 
-/** Cheque se a versão do node.js é a 8.0.0 ou acima */
-if (process.version.slice(1).split('.')[0] < 8) throw new Error('Node 8.0.0 or higher is required. Update Node on your system.');
+require('dotenv').config() 
+
+const Discord = require('discord.js') 
+
+const { config } = require("dotenv");
+const { readdirSync } = require('fs')
+const Enmap = require('enmap')
+const client = new Discord.Client() 
+const ascii = require("ascii-table");
+
 
 client.commands = new Enmap()
+client.startTime = Date.now()
 
 config ({
     path: __dirname + "/.env"
 });
 
 ["command"].forEach(handler => {
-  require(`./handler/${handler}`)(client);
+    require(`./handler/${handler}`)(client);
 });
 
 client.on ("ready", () => {
@@ -28,28 +34,29 @@ client.on ("ready", () => {
     });
 });
 
+
 client.on("message", async message => {
-  const prefix = "!";
-
-  if (message.author.bot) return;
-  if (!message.guild) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  // If message.member is uncached, cache it.
-  if (!message.member) message.member = await message.guild.fetchMember(message);
-
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
+    const prefix = "!";
   
-  if (cmd.length === 0) return;
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
   
-  // Get the command
-  const command = client.commands.get(cmd);
-  // If none is found, try to find it by alias
-
-  // If a command is finally found, run the command
-  if (command) 
-      command.run(client, message, args);
-});
-
-client.login(process.env.AUTH_TOKEN);
+    // If message.member is uncached, cache it.
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+  
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    // Get the command
+    const command = client.commands.get(cmd);
+    // If none is found, try to find it by alias
+  
+    // If a command is finally found, run the command
+    if (command) 
+        command.run(client, message, args);
+    });
+    
+client.login(process.env.AUTH_TOKEN)
